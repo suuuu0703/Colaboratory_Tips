5c8a2808e400816f04b8ab17a48c77740421ebd5
suuuu0703 <takayuki.suzuki.0703@gmail.com>
2021-08-17 13:58:30 +0900

    Update 日付列の生成.sql

--- mysql/日付列の生成.sql
+++ mysql/日付列の生成.sql
@@ -1,1 +1,11 @@
-
+select
+    date_format(date_add('2021-03-31', interval td.generate_series day), '%Y-%m-01') as month
+from (
+    SELECT 0 generate_series FROM DUAL WHERE (@num:=1-1)*0 
+    UNION ALL
+    SELECT @num:=@num+1 FROM `information_schema`.COLUMNS LIMIT 1000000
+    ) as td
+group by
+    1
+having
+    month <= current_date
